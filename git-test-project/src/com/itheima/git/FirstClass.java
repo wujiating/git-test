54a8aead78d0d8258bf91d65d535f48b98b60f7a
zhangsan <zhangsan@itcast.cn>
2020-06-09 21:22:38 +0800

    多少度

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -4,5 +4,7 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("第一次");
         System.out.println("第二次");
+
+        System.out.println("远程clone下来后");
     }
 }
